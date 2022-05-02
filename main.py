7de5ae1257d83af6e1f1d7351aff1643b464db05
sasha <pavlov.as@phystech.edu>
2022-05-03 01:04:32 +0300

    add a check for the correctness of initial data

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import argparse
 from PIL import Image
 from pytesseract import pytesseract
 
+from correctness import check_correctness
 
-def parse_args() -> argparse.Namespace:
+
+def parse_args() -> tuple:
     parser = argparse.ArgumentParser(description="Recognize text in image.")
     parser.add_argument(
         "text_line_drawer",
@@ -23,31 +25,38 @@ def parse_args() -> argparse.Namespace:
         help="Path to output document"
     )
 
-    recognizing_args = parser.parse_args()
-    return recognizing_args
+    args = parser.parse_args()
+    return args.input_file, args.output_file, args.text_line_drawer
 
 
-def create_png(input_img: Image, output_file: str) -> None:
-    pass
+def create_png(input_file: str, output_file: str) -> None:
+    input_img = cv2.imread(input_file)
 
 
-def create_txt(input_img: Image, output_file: str) -> None:
-    pass
+def create_txt(input_file: str, output_file: str) -> None:
+    input_img = Image.open(input_file)
+    text = pytesseract.image_to_string(input_img)
+
+    with open(output_file, mode="w", encoding="utf-8") as res_file:
+        res_file.write(text)
 
 
-def recognizing() -> None:
-    args = parse_args()
-
-    input_img = Image.open(args.input_file)
-    output_file = args.output_file
-
-    if args.text_line_drawer == "opencv":
-        create_png(input_img, output_file)
-    elif args.test_line_drawer == "tesseract":
-        create_txt(input_img, output_file)
+def recognizing(drawer: str, input_file: str, output_file: str) -> None:
+    if drawer == "opencv":
+        create_png(input_file, output_file)
+    elif drawer == "tesseract":
+        create_txt(input_file, output_file)
 
 
 if __name__ == "__main__":
-    recognizing()
+    # parse args for recognizing
+    input_file, output_file, drawer = parse_args()
+
+    # checking the correctness of the entered data
+    check_res = check_correctness(input_file, output_file)
+
+    if check_res:
+        # recognizing data
+        recognizing(drawer, input_file, output_file)
 
 
